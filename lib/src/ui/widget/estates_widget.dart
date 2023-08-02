29c10524e541b54aa0002f6a208ae2ef1b65abf9
kozimjonmirzaqosimov <kozimjonmirzaqosimov@gmail.com>
2023-08-02 12:19:09 +0500

    7

--- lib/src/ui/widget/estates_widget.dart
+++ lib/src/ui/widget/estates_widget.dart
@@ -6,55 +6,57 @@ class EstateWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 70),
-      child: Column(
-        children: [
-          Container(
-            width: 270,
-            height: 160,
-            decoration: BoxDecoration(
-                color: Colors.green, borderRadius: BorderRadius.circular(10)),
-            child: Row(
-              children: [
-                Stack(
-                  children: [
-                    Container(
-                      width: 140,
-                      height: 150,
-                      child: Image.asset("assets/images/shape.png"),
-                    ),
-                    Positioned(
-                      left: 10,
-                      top: 12,
-                      child: Container(
-                        width: 35,
-                        height: 35,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            color: Colors.white70),
-                        child: Icon(Icons.favorite_outline),
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 70),
+        child: Column(
+          children: [
+            Container(
+              width: 270,
+              height: 160,
+              decoration: BoxDecoration(
+                  color: Colors.green, borderRadius: BorderRadius.circular(10)),
+              child: Row(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        width: 140,
+                        height: 150,
+                        child: Image.asset("assets/images/shape.png"),
                       ),
-                    ),
-                    Positioned(
-                      bottom: 10,
-                      left: 10,
-                      child: Container(
-                        width: 75,
-                        height: 35,
-                        decoration: BoxDecoration(
-                            color: Color(0XFF234F68),
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Text("Apartment",style: TextStyle(
-                            fontSize: 12, fontWeight: FontWeight.w700,color: Colors.white),),
+                      Positioned(
+                        left: 10,
+                        top: 12,
+                        child: Container(
+                          width: 35,
+                          height: 35,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: Colors.white70),
+                          child: Icon(Icons.favorite_outline),
+                        ),
                       ),
-                    )
-                  ],
-                ),
-              ],
+                      Positioned(
+                        bottom: 10,
+                        left: 10,
+                        child: Container(
+                          width: 75,
+                          height: 35,
+                          decoration: BoxDecoration(
+                              color: Color(0XFF234F68),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Text("Apartment",style: TextStyle(
+                              fontSize: 12, fontWeight: FontWeight.w700,color: Colors.white),),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
